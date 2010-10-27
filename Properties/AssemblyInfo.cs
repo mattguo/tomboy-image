a0534ac1a03ea436777733010603bdc9ed7bb15c
Matt Guo <matt@mattguo.com>
2010-10-27 14:32:57 +0800

    AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("Tomboy.InsertImage")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Matt Guo")]
+[assembly: AssemblyProduct ("Tomboy.InsertImage")]
+[assembly: AssemblyCopyright ("Copyright © Matt Guo 2010")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+[assembly: AssemblyVersion ("0.2.0.10")]
+[assembly: AssemblyFileVersion ("0.2.0.10")]
